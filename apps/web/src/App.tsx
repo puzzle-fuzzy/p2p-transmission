0ebd8e1bd5ce2743288749c442a06b5d47fa03d6
18267094443 <18267094443@163.com>
2026-07-11 10:32:36 +0800

    chaneg

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="h-svh bg-[#2d2d2d]">
+      <div>
+        <div className="rounded-full w-20 h-20">
+          <img src="/vite.svg" alt="" />
+        </div>
+        <div className="rounded-full w-20 h-20">
+          <img src="/vite.svg" alt="" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
